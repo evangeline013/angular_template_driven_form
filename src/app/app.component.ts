02b0f28e52a284eef84baa7f2af2581b3cd930dc
Eva <evangeline013@gmail.com>
2019-10-02 07:38:50 -0400

    use form data after the form is submitted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -29,7 +37,7 @@ export class AppComponent {
       }
     })
     // patchValue: overwrites the field specified, on available on the form level
-    // setValue: overwrites the value for the whole form, available on the ngForm level(signupForm) and form level. 
+    // setValue: overwrites the value for the whole form, available on the ngForm level(signupForm) and form level.
   }
 
   // onSubmit(form: NgForm) {
@@ -38,5 +46,11 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
